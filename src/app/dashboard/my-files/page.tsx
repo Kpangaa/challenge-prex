5fba120061bf0d49434c5c5e87b6d69a151650d3
Kpangaa <hernan.moya16@gmail.com>
2023-12-13 04:37:10 -0300

    added download files

--- src/app/dashboard/my-files/page.tsx
+++ src/app/dashboard/my-files/page.tsx
@@ -23,6 +23,7 @@ function MyFiles() {
   const [emailShare, setEmailShare] = useState("");
   const [urlShare, setUrlShare] = useState("");
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const CLOUDINARY_URL = "https://res.cloudinary.com/dqcz2mhzo/image/upload/";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -61,9 +62,34 @@ function MyFiles() {
         method: "POST",
         body: formData,
       });
-      console.log("🚀 ~ file: page.tsx:61 ~ onShareFile ~ resp:", resp);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const onDownloadFile = async () => {
+    try {
+      const newUrl = CLOUDINARY_URL + "fl_attachment/";
+      const file = urlShare.slice(CLOUDINARY_URL.length);
+      const fileName = file.split("/").pop();
+      if (fileName) {
+        const aTag = document.createElement("a");
+        aTag.href = newUrl + file;
+        aTag.setAttribute("download", fileName);
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onDeleteFile = async () => {
+    try {
+      
+    } catch (error) {
+    
     }
   };
 
@@ -123,15 +149,35 @@ function MyFiles() {
                   width={100}
                   height={100}
                 />
-                <Button
-                  onPress={() => {
-                    setUrlShare(image);
-                    onOpen();
-                  }}
-                  color="primary"
-                >
-                  Open Modal
-                </Button>
+                <div>
+                  <Button
+                    onPress={() => {
+                      setUrlShare(image);
+                      onOpen();
+                    }}
+                    color="primary"
+                  >
+                    Share
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      setUrlShare(image);
+                      onDownloadFile();
+                    }}
+                    color="default"
+                  >
+                    DO
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      setUrlShare(image);
+                      onOpen();
+                    }}
+                    color="warning"
+                  >
+                    DE
+                  </Button>
+                </div>
               </div>
             ))
           ) : (
